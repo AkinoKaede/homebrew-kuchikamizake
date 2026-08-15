9517c2a4f6419b47a924921840f4ec197ecc47fb
秋野かえで <26006258+AkinoKaede@users.noreply.github.com>
2026-08-15 03:08:28 +0000

    kelivo 1.2.2

--- Casks/kelivo.rb
+++ Casks/kelivo.rb
@@ -1,6 +1,6 @@
 cask "kelivo" do
-  version "1.2.1,64"
-  sha256 "b83beaadc3e71a20d82d4d58ad23d829aa3eb28cab8e7929999e010a33a8c28d"
+  version "1.2.2,66"
+  sha256 "b6d5e50f6d8db2db3b2c7d5e11e258f294fbdac6f062cf03008b3cdcb4b2e5e8"
 
   url "https://github.com/Chevey339/kelivo/releases/download/v#{version.csv.first}/Kelivo_macos_#{version.csv.first}%2B#{version.csv.second}.dmg",
       verified: "github.com/Chevey339/kelivo/"
